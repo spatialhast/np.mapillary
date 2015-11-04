b3c276024911ec6d0e69de1a8361d03e66627612
spatialhast <a.hast@mail.ru>
2015-11-04 13:49:01 +0200

    update

--- main.js
+++ main.js
@@ -361,6 +361,104 @@ map = (function () {
         layer.addTo(map);
     });
 
+	
+	
+var nature_conservation_polygon = L.geoJson(null, {
+    style: function(feature) {
+        if (feature.properties.name === null) {
+            return {
+                color: "#ff4c4c",
+                fill: true,
+                opacity: 0.7,
+                weight: 2
+            };
+        } else
+            return {
+                color: "#9ace00",
+                fill: true,
+                opacity: 0.7,
+                weight: 2
+            };
+    },
+    onEachFeature: function(feature, layer_nature_conservation_polygon) {
+        /*if (feature.properties) {
+                    layer_nature_conservation_polygon.bindPopup(feature.properties.name, {
+                        closeButton: true,
+                        minWidth: 120,
+                        minHeight: 25
+                    });
+            }*/
+        layer_nature_conservation_polygon.on({
+            mouseover: function(e) {
+                var layer_nature_conservation_polygon = e.target;
+                layer_nature_conservation_polygon.setStyle({
+                    weight: 3,
+                    color: "#00FFFF",
+                    opacity: 1
+                });
+                if (!L.Browser.ie && !L.Browser.opera) {
+                    layer_nature_conservation_polygon.bringToFront();
+                }
+
+				var name = feature.properties.name == null ? '' : '<b>name: </b>' + feature.properties.name + '<br>';
+				var name_uk = feature.properties.name_uk == null ? '' : '<b>name_uk: </b>' + feature.properties.name_uk + '<br>';
+				var name_ru = feature.properties.name_ru == null ? '' : '<b>name_ru: </b>' + feature.properties.name_ru + '<br>';
+				var name_en = feature.properties.name_en == null ? '' : '<b>name_en: </b>' + feature.properties.name_en + '<br>';
+				var boundary = feature.properties.boundary == null ? '' : '<b>boundary: </b>' + feature.properties.boundary + '<br>';
+				var prot_class = feature.properties.prot_class == null ? '' : '<b>prot_class: </b>' + feature.properties.prot_class + '<br>';
+				var prot_stat = feature.properties.prot_stat == null ? '' : '<b>prot_stat: </b>' + feature.properties.prot_stat + '<br>';
+				var prot_title = feature.properties.prot_title == null ? '' : '<b>prot_title: </b>' + feature.properties.prot_title + '<br>';
+				var naturalt = feature.properties.naturalt == null ? '' : '<b>natural: </b>' + feature.properties.naturalt + '<br>';
+				var amenity = feature.properties.amenity == null ? '' : '<b>amenity: </b>' + feature.properties.amenity + '<br>';
+				var leisure = feature.properties.leisure == null ? '' : '<b>leisure: </b>' + feature.properties.leisure + '<br>';
+				var landuse = feature.properties.landuse == null ? '' : '<b>landuse: </b>' + feature.properties.landuse + '<br>';
+				var historic = feature.properties.historic == null ? '' : '<b>historic: </b>' + feature.properties.historic + '<br>';
+				var tourism = feature.properties.tourism == null ? '' : '<b>tourism: </b>' + feature.properties.tourism + '<br>';
+				var operator = feature.properties.operator == null ? '' : '<b>operator: </b>' + feature.properties.operator + '<br>';
+				var place = feature.properties.place == null ? '' : '<b>place: </b>' + feature.properties.place + '<br>';
+				var website = feature.properties.website == null ? '' : '<b>website: </b>' + feature.properties.website + '<br>';
+				var wikipedia = feature.properties.wikipedia == null ? '' : '<b>wikipedia: </b>' + feature.properties.wikipedia + '<br>';
+				var note = feature.properties.note == null ? '' : '<b>note: </b>' + feature.properties.note + '<br>';
+				var area = feature.properties.area == null ? '' : '<b>area: </b>' + feature.properties.area + ' sq. km';
+
+			    var content = name + name_uk + name_ru + name_en + boundary + prot_class + prot_stat + prot_title + naturalt + amenity + leisure + landuse + historic + tourism + operator + place + website + wikipedia + note + area;
+
+                nature_conservation_polygon.bindLabel(content + '<br>' + '<b style = "color: green">Use double-click to move on http://www.openstreetmap.org page</b>', {
+                    direction: 'auto'
+                }).addTo(map);
+		
+/*				
+                if (feature.properties.name === null) {
+                    nature_conservation_polygon.bindLabel('No name', {
+                        direction: 'auto'
+                    }).addTo(map);
+                } else
+                    nature_conservation_polygon.bindLabel(feature.properties.name, {
+                        direction: 'auto'
+                    }).addTo(map);
+*/					
+	
+            },
+            mouseout: function(e) {
+                nature_conservation_polygon.resetStyle(e.target);
+            },
+            dblclick: function(e) {
+				var url = 'http://www.openstreetmap.org/' + (feature.properties.osm_id < 0 ? 'relation/' + feature.properties.osm_id * (-1) : 'way/' + feature.properties.osm_id);
+				window.open(url);
+            }
+        });
+    }
+});
+$.getJSON("data/nature_conservation_polygon.geojson", function(data) {
+    nature_conservation_polygon.addData(data);
+});	
+	
+nature_conservation_polygon.addTo(map);	
+	
+	
+	
+	
+	
     return map;
 
 }());
